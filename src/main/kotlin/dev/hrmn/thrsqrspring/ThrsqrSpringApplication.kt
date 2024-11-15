a1e4dba2a9a7bc98ba9a9b382a68cf492c467fd7
marc-hermann <marc.hermann@adevinta.com>
2024-11-15 14:09:55 +0100

    set up session handling

--- src/main/kotlin/dev/hrmn/thrsqrspring/ThrsqrSpringApplication.kt
+++ src/main/kotlin/dev/hrmn/thrsqrspring/ThrsqrSpringApplication.kt
@@ -3,8 +3,10 @@ package dev.hrmn.thrsqrspring
 import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession
 
 @SpringBootApplication
+@EnableJdbcHttpSession
 class ThrsqrSpringApplication
 
 fun main(args: Array<String>) {
