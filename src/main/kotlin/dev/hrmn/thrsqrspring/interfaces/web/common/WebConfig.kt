77dcb56bb07eb79922770df6510346bfd96cdfd0
marc-hermann <marc.hermann@adevinta.com>
2024-11-15 16:37:05 +0100

    add annotation

--- src/main/kotlin/dev/hrmn/thrsqrspring/interfaces/web/common/WebConfig.kt
+++ src/main/kotlin/dev/hrmn/thrsqrspring/interfaces/web/common/WebConfig.kt
@@ -1,8 +1,10 @@
 package dev.hrmn.thrsqrspring.interfaces.web.common
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(SessionInterceptor())
