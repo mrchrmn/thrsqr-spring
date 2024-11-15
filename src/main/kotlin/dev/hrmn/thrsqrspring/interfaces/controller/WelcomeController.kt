3cd534fb0c4d757be4121f09d607983776d3dcc7
marc-hermann <marc.hermann@adevinta.com>
2024-11-15 15:12:22 +0100

    read language from session

--- src/main/kotlin/dev/hrmn/thrsqrspring/interfaces/controller/WelcomeController.kt
+++ src/main/kotlin/dev/hrmn/thrsqrspring/interfaces/controller/WelcomeController.kt
@@ -2,6 +2,8 @@ package dev.hrmn.thrsqrspring.interfaces.controller
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpSession
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,11 +13,25 @@ import java.nio.file.Paths
 @Controller
 class WelcomeController {
     @GetMapping("/")
-    fun welcome(model: Model): String {
+    fun welcome(
+        model: Model,
+        session: HttpSession,
+        request: HttpServletRequest,
+    ): String {
+        var sessionLanguage = session.getAttribute("language") as String?
+        if (sessionLanguage == null) {
+            sessionLanguage = request.getHeader("accept-Language")?.substring(0, 2) ?: "en"
+            session.setAttribute("language", sessionLanguage)
+        }
+
+        var localizedLanguage = "en"
+        if (sessionLanguage == "de") {
+            localizedLanguage = "de"
+        }
+
         val texts = readTextsJson()
-        val language = "de" // or "en"
-        val localizedTexts = texts["texts"]?.get(language)
-        model.addAttribute("language", language)
+        val localizedTexts = texts["texts"]?.get(localizedLanguage)
+        model.addAttribute("language", sessionLanguage)
         model.addAttribute("TEXTS", localizedTexts)
         return "welcome"
     }
