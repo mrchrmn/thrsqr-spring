0b731ef9b9d8b6a180685215c4e74d955fe8cd3f
marc-hermann <marc.hermann@adevinta.com>
2024-11-29 19:35:06 +0100

    add dto

---
+++ src/main/kotlin/dev/hrmn/thrsqrspring/adapter/input/web/dto/NewEventForm.kt
@@ -0,0 +1,12 @@
+package dev.hrmn.thrsqrspring.adapter.input.web.dto
+
+data class NewEventForm(
+    val eventTitle: String,
+    val eventDayOfWeek: Int,
+    val eventTime: String,
+    val eventTimeZone: String,
+    val eventInfo: String?,
+    val eventLogoURL: String,
+    val email: String?,
+    val message: String?
+)
