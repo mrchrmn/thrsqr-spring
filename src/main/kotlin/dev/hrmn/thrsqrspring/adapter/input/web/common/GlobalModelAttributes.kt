6853d216acbd402326be9ad514c55cb85c09be32
marc-hermann <marc.hermann@adevinta.com>
2024-12-27 18:18:57 +0100

    add origin to global attributes

--- src/main/kotlin/dev/hrmn/thrsqrspring/adapter/input/web/common/GlobalModelAttributes.kt
+++ src/main/kotlin/dev/hrmn/thrsqrspring/adapter/input/web/common/GlobalModelAttributes.kt
@@ -8,4 +8,12 @@ import org.springframework.web.bind.annotation.ModelAttribute
 class GlobalModelAttributes {
     @ModelAttribute("currentURL")
     fun currentUrl(request: HttpServletRequest): StringBuffer = request.requestURL
+
+    @ModelAttribute("origin")
+    fun origin(request: HttpServletRequest): String {
+        val scheme = request.scheme
+        val serverName = request.serverName
+        val serverPort = request.serverPort
+        return "$scheme://$serverName:$serverPort"
+    }
 }
